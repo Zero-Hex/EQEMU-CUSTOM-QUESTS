33e397ed7784d1729c473e281e0077e43cb7eefd
Zero-Hex <67660381+Zero-Hex@users.noreply.github.com>
2025-11-14 15:10:08 -0600

    Refactor EVENT_SAY logic for parcel commands

--- Parcel-Manager/global/global_player.pl
+++ Parcel-Manager/global/global_player.pl
@@ -5,16 +5,9 @@
 # Subroutines to Display and Redeem Parcels
 # =========================================================================
 sub EVENT_SAY {
-    quest::debug("EVENT_SAY triggered with text: " . (defined $text ? $text : "undef"));
-
     # Check 1: Handle the initial !parcel command (typed by the player)
-    if (defined $text && $text eq "!parcel") {
-        plugin::DisplayParcels();
-        return;
-    }
-
     # Check 2: Handle !parcel reclaim <ID> command (from clickable link or typed)
-    if (defined $text && $text =~ /^!parcel reclaim (\d+)$/) {
+    if ($text =~ /^!parcel reclaim (\d+)$/) {
         my $parcel_id = $1;
         quest::debug("RedeemParcel called with parcel_id: $parcel_id");
         plugin::RedeemParcel($parcel_id);
@@ -22,19 +15,13 @@ sub EVENT_SAY {
     }
 
     # Check 3: Handle !reclaim all command
-    if (defined $text && $text eq "!reclaim all") {
+    if ($text eq "!reclaim all") {
         plugin::ReclaimAllParcels();
         return;
     }
-}
-
-sub EVENT_ITEM_CLICK_CAST {
-    my $item_id = ($itemid || 0);
-    my $spell_id = ($spell_id || 0);
-    quest::debug("EVENT_ITEM_CLICK_CAST: item=$item_id, spell=$spell_id, text=" . (defined $text ? $text : "undef"));
-}
-
-sub EVENT_CLICKDOOR {
-    quest::debug("EVENT_CLICKDOOR triggered");
+        elsif ($text eq "!parcel") {
+        plugin::DisplayParcels();
+        return;
+    }
 }
 
